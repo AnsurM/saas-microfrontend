22830e053bb49655f23f42097a47d432cb49e2d6
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-03 20:34:55 +0200

    integrate marketing into container, share deps

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 const App = () => {
-  return <h1>Container here</h1>;
+  return (
+    <div>
+      <h1>Container here</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
 
 export default App;
