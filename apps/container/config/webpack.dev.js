eb0d94cead75457c227096d4c0826d8ea6f8985b
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-03 21:09:27 +0200

    setup automatic shared deps in webpack config

--- apps/container/config/webpack.dev.js
+++ apps/container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
 
 const commonConfig = require("./webpack.common");
 
@@ -21,7 +22,7 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
   ],
 };
--- apps/marketing/config/webpack.dev.js
+++ apps/marketing/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
 
 const commonConfig = require("./webpack.common");
 
@@ -22,7 +23,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-      shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
   ],
 };
