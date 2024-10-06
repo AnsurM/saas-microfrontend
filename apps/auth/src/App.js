8a9c545286d8437acd0eee3630245206695cb0dc
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-06 04:43:45 +0200

    setup and  integrate auth app

---
+++ apps/auth/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import SignUp from "./components/Signup";
+import SignIn from "./components/Signin";
+
+const generateClassName = createGenerateClassName({ productionPrefix: "au" });
+
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/auth/signup" component={SignUp} />
+            <Route exact path="/auth/signin" component={SignIn} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
