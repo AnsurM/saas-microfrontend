59e5023582693e2fbe0ad02f4261dcf1c8ff4711
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-06 21:23:49 +0200

    add basic signIn, signOut logic

--- apps/auth/src/App.js
+++ apps/auth/src/App.js
@@ -9,14 +9,18 @@ import SignIn from "./components/Signin";
 
 const generateClassName = createGenerateClassName({ productionPrefix: "au" });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
           <Switch>
-            <Route exact path="/auth/signup" component={SignUp} />
-            <Route exact path="/auth/signin" component={SignIn} />
+            <Route exact path="/auth/signup">
+              <SignUp onSignIn={onSignIn} />
+            </Route>
+            <Route exact path="/auth/signin">
+              <SignIn onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </StylesProvider>
       </Router>
--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
@@ -12,14 +12,25 @@ const AuthApp = lazy(() => import("./components/AuthApp"));
 const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth" component={AuthApp} />
+              <Route path="/auth">
+                <AuthApp
+                  onSignIn={() => {
+                    setIsSignedIn(true);
+                  }}
+                />
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
