0e775353694cd208cbd8886f74409634a4bfcaea
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-06 02:35:02 +0200

    update css prodPrefix

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -4,18 +4,18 @@ import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
-const generateClassName = createGenerateClassName({ productionPrefix: "ca" });
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 const App = () => {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
         </div>
-      </BrowserRouter>
-    </StylesProvider>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
 
